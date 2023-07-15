c08ffa65272c1a7a947884d0ccfbd0aadcad2d46
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-15 11:36:20 +0530

    Create sort_colors.java

---
+++ Arrays/sort_colors.java
@@ -0,0 +1,7 @@
+//Salesforce
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+}
+//as red=0 white=1 and blue=2 we need to arrrange all reds then white and then blue so just sort array
