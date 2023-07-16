8f938ba65a17bf3d5963a47af5bbab917b27a6cc
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-16 20:12:48 +0530

    Create index_of_first_occurrence.java

---
+++ Strings/index_of_first_occurrence.java
@@ -0,0 +1,7 @@
+//salesforce
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
+//indexOf return index of first occurenece of substring in string else returns -1
